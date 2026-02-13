d857175bc9bcb8a849f17553b8f72ee66a660103
myo <legendmyota@gmail.com>
2026-02-13 16:52:18 -0500

    fix

--- full code.py
+++ full code.py
@@ -61,24 +61,26 @@ for filename in os.listdir(folder):
 this is Arij's code
 I will use certain terminology that we will modify once we put everything together
 '''
-def HaversineFormula(lat1, long1, lat2, long2):
-  radlat1 = math.radians(lat1)
-  radlat2 = math.radians(lat2)
-  radlong1 = math.radians(long1)
-  radlong2 = math.radians(long2)
-
-  dlat = radlat2 - radlat1
-  dlong = radlong2 - radlong1
-
-  haversine = math.sin(dlat/2)**2 + math.cos(radlat1) * math.cos(radlat2) * math.sin(dlong/2)**2
-  angle = 2 * math.atan2(math.sqrt(haversine),math.sqrt(1-haversine))
-  distance = 6371 * angle
-
-  return distance
-
-def SpeedFormula(distance, time):
-  speed = distance/time
-  return speed
+if len(results) == 2:
+    def HaversineFormula(lat1, long1, lat2, long2):
+        radlat1 = math.radians(lat1)
+        radlat2 = math.radians(lat2)
+        radlong1 = math.radians(long1)
+        radlong2 = math.radians(long2)
+        
+        dlat = radlat2 - radlat1
+        dlong = radlong2 - radlong1
+        
+        haversine = math.sin(dlat/2)**2 + math.cos(radlat1) * math.cos(radlat2) * math.sin(dlong/2)**2
+        angle = 2 * math.atan2(math.sqrt(haversine),math.sqrt(1-haversine))
+        distance = 6371 * angle
+        
+        return distance
+    def SpeedFormula(distance, time):
+        speed = distance/time
+        return speed
+    
+    results.pop(0)
 
 '''
 This is Yui's code
