cf6637e19849ae0d0be80e02f2de3b0bbc6f6a87
honeydoop <danah.dezeme@dawsoncollege.qc.ca>
2026-02-13 14:08:15 -0500

    10 mins stop & sig fig trim
    Added a timed loop that runs for 9 minutes and prints a message every 10 seconds. Included code for speed trimming to 7 significant figures.

--- full code.py
+++ full code.py
@@ -1,5 +1,27 @@
 #add you code here!
-print('test')
+
+'''
+This is Danah's code
+One is for the code to stop after 10 mins
+The other is for trimming sig figs
+'''
+from datetime import datetime, timedelta
+from time import sleep
+
+start_time = datetime.now()
+now_time = datetime.now()
+
+while (now_time < start_time + timedelta(minutes=9)): #minutes != 10 b/c code could take longer
+    print("The rest of the code comes here✨✨")
+    sleep(10) #the interval from which the 'hello' gets sent
+    now_time = datetime.now()
+
+
+speed = 7.74686573629 #this is only an example
+str_speed = str(speed)
+final_speed = str_speed[0:7]
+print(final_speed
+
 '''
 this is Arij's code
 I will use certain terminology that we will modify once we put everything together
